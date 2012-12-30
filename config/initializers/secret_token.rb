d63ccccab763cd55324713eb77fb21e25b04b397
root <root@JamesDebianDevServer>
2012-12-30 14:54:45 +1300

    Initial application skeleton. Got to start somewhere.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReceiptCapture::Application.config.secret_token = '061dba18abfabe54ae87285a3aa8c0a73dabbcf4d959601c17cbb25cb9a5da49a4feb4621e853fff398c1e6583bac115dd2e02f0b906f7ce664e1b67e6ec669e'
